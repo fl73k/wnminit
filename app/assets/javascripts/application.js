d9834ed6d07fd22a7c6de194b1121fd86ab5ca36
altanium <alexeitanurcov@mial.ru>
2017-06-02 12:02:15 +0300

    Task 2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,3 +12,15 @@
 //
 //= require rails-ujs
 //= require_tree .
+ function showHide(element_id)
+         {
+             if (document.getElementById(element_id))
+             { 
+                 var obj = document.getElementById(element_id); 
+                 if (obj.style.display != "block")
+                 { 
+                     obj.style.display = "block";
+                 }
+                 else obj.style.display = "none";
+             }
+         }
